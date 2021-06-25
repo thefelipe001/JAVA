b39f5afc94bb6843e959eed546ff87508db87950
thefelipe001 <elfelipe_lugo@hotmail.com>
2021-06-25 18:46:12 -0400

    felipe

---
+++ src/java/Modelo/Persona.java
@@ -0,0 +1,38 @@
+
+package Modelo;
+
+public class Persona {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    int id;
+    String dni;
+    String nom;
+
+    public Persona() {
+    }
+
+   
+    
+}
